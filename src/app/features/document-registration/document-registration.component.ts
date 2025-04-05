a5e8bcb802a64a6fed0044194da1e89110c1b6c1
Olimjonov O <almjnvfsystem@gmail.com>
2025-04-05 18:54:32 +0500

    feat: enhance DocumentRegistration component with document table and action buttons; update styles

--- src/app/features/document-registration/document-registration.component.ts
+++ src/app/features/document-registration/document-registration.component.ts
@@ -1,14 +1,46 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Button } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { Toolbar } from 'primeng/toolbar';
-
+import { Correspondent, DeliveryMethod } from '../../enums';
+import { IncomingDocument } from '../../models/incoming-document';
 @Component({
   selector: 'app-document-registration',
-  imports: [Toolbar, Button],
+  imports: [Toolbar, Button, TableModule, DatePipe],
   templateUrl: './document-registration.component.html',
   styleUrl: './document-registration.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DocumentRegistrationComponent {
+export class DocumentRegistrationComponent implements OnInit {
+
+  documents: IncomingDocument[] = [];
+
+  ngOnInit(): void {
+    this.loadDocuments();
+  }
+
+  loadDocuments(): void {
+    this.documents = [
+      {
+        regNumber: '12345',
+        regDate: new Date(),
+        deliveryMethod: DeliveryMethod.Email,
+        correspondent: Correspondent.CB,
+        topic: 'Project Update',
+        access: true,
+        control: false,
+      },
+      {
+        regNumber: '67890',
+        regDate: new Date(),
+        deliveryMethod: DeliveryMethod.Email,
+        correspondent: Correspondent.HOA,
+        topic: 'Meeting Minutes',
+        access: false,
+        control: true,
+      }
+    ];
+  }
 
 }
