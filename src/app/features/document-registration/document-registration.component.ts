1c2f448dbfbeeea99a056beed24dac51ae7d7d0a
Olimjonov O <almjnvfsystem@gmail.com>
2025-04-05 18:06:28 +0500

    feat: add DocumentRegistration component with PrimeNG styles and routing

---
+++ src/app/features/document-registration/document-registration.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-registration',
+  imports: [],
+  templateUrl: './document-registration.component.html',
+  styleUrl: './document-registration.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DocumentRegistrationComponent {
+
+}
