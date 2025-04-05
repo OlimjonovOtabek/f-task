e1d935ee8b72406a97dd5c03f8f55a3a447ed579
Olimjonov O <almjnvfsystem@gmail.com>
2025-04-05 17:49:37 +0500

    feat: add enums for Correspondent and DeliveryMethod; create IncomingDocument interface

---
+++ src/app/enums/correspondent.enum.ts
@@ -0,0 +1,5 @@
+export enum Correspondent {
+  CB = 'ЦБ',
+  GNI = 'ГНИ',
+  HOA = 'ТСЖ',
+}
---
+++ src/app/models/incoming-document.ts
@@ -0,0 +1,17 @@
+import { Correspondent, DeliveryMethod } from "../enums";
+
+export interface IncomingDocument {
+  regNumber: string;
+  regDate: Date;
+  outgoingNumber?: string;
+  outgoingDate?: Date;
+  deliveryMethod: DeliveryMethod;
+  correspondent: Correspondent;
+  topic: string;
+  description?: string;
+  executionDate?: Date;
+  access: boolean;
+  control: boolean;
+  file?: File | null;
+  filePath?: string;
+}
